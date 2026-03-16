068b6ce261bcfc461968b6537f2e07787764dbb2
b0966836079-afk <b0966836079@gmail.com>
2026-03-16 07:39:26 +0000

    ai

--- test.py
+++ test.py
@@ -1,11 +1,19 @@
+import os
 from google import genai
+from dotenv import load_dotenv
 
-# 初始化 Client
-client = genai.Client(api_key="AIzaSyDk5xmS_YkESRDxYJErNZqWZM_RfbiPHpQ")
+# 1. 讀取 .env 檔案
+load_dotenv() 
 
-# 測試生成內容
+# 2. 從環境變數抓取金鑰
+api_key = os.getenv("GEMINI_API_KEY")
+
+# 3. 初始化 Client
+client = genai.Client(api_key=api_key)
+
+# 4. 測試生成內容
 response = client.models.generate_content(
-    model="gemini-2.5-flash", 
+    model="gemini-2.0-flash",
         contents="分析這段話的心情：『專題快做不完了好焦慮』"
         )
 
