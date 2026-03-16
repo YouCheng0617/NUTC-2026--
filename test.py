52ef0643b0c9ec3c5fdc0dbeb0f0e7eaac38e87a
b0966836079-afk <b0966836079@gmail.com>
2026-03-16 06:48:25 +0000

    心情分析

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ client = genai.Client(api_key="AIzaSyDk5xmS_YkESRDxYJErNZqWZM_RfbiPHpQ")
 
 # 測試生成內容
 response = client.models.generate_content(
-    model="gemini-2.0-flash", 
+    model="gemini-2.5-flash", 
         contents="分析這段話的心情：『專題快做不完了好焦慮』"
         )
 
